8834fbf59ff437f495f0ed9a7434441766554bc4
osunadan <osuna_dan@outlook.com>
2023-06-14 19:15:40 -0600

    Update Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "~": path.resolve(__dirname, "node_modules"),
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
   build: {
     chunkSizeWarningLimit: 1600,
   },
